d1cf9155bc3e79e82fb61f34ca6e60ca61e0f554
Vikram Narra <vchandra.narra@gmail.com>
2022-02-21 15:29:49 -0500

    gamplay pseduo code

---
+++ gamplay.js
@@ -0,0 +1,117 @@
+//Ligerr
+//Created 2022 Feb 16
+ 
+
+// FEN String
+// "TLL2LLTLL3TLLL 1"
+// The last number tells us who's turn it is
+// 1 - Tiger
+// 0 - Lamb
+ 
+function display(board) {
+    // Crate a sample line
+    // Design Board
+    // Design Board Pieces
+    // Design the animations of the pieces
+  
+    // Develop the connection between the
+    // 2d mapping of the pieces to the board
+  
+    // Visually display Forsyth-Edwards Notation
+  
+  
+    // *** This function will not return anything,
+    // it will be a passive function that will only print
+    // the current status of the board
+  
+ }
+
+ function status(board) {
+  
+    // Return the status of the board
+    // 1 - Tiger Win
+    // 0 - Tie
+    // -1 - Lambs Win
+  
+
+    // Convert the FEN string into readable game logic.
+    // If Tiger can't move -> Lambs Win
+    // If Tiger >= Lambs -> Tiger Wins
+    // If a move is repeated 3 times -> Tie
+
+ }
+  
+ function convertToReadable(board) {
+  
+    // This function will return an array which will map every
+    // piece to a 5x6 array. Where only 5 indexes will have the values 0.
+    // Denoting that there is a boarder
+    
+    // [0, 0, L, 0, 0, 0]
+    // [L, T, L, L, T, L]
+    // [L, T, L, E, E, L]
+    // [L, E, L, L, L, E]
+    // [L, L, L, E, L, L]
+    // E - Represents an Empty spot.
+  
+ }
+  
+ 
+  
+  
+ function makemove(board) {
+  
+    // ** This function will not return anything,
+  
+    // it will change the arangement of the board.
+  
+    //
+  
+ }
+  
+ 
+  
+  
+ function determineoptimal(board) {
+  
+    // ** This function will determine what the most
+  
+    // optimal move would be given any current position
+  
+ }
+  
+ 
+  
+  
+ 
+  
+  
+ function createdatabase() {
+  
+    // This module will be connected to the SQL database,
+  
+    // and store the key transport modules that need to be sent
+  
+    // to the databse
+  
+ 
+  
+  
+    // FEN
+  
+    // Time
+  
+    // Rating of Player
+  
+ }
+  
+ function gameplay() {
+  
+    // Will be the mastermind behind the game
+  
+    // Will play the m
+  
+ }
+ 
+  
+ 
