b927588c3729a87b98e49f6466b581a258b046a8
Vikram Narra <vchandra.narra@gmail.com>
2022-02-22 22:09:05 -0500

    images added

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //Ligerr
 //Created 2022 Feb 16
  
-
 // FEN String
 // "TLL2LLTLL3TLLL 1"
 // "LLTLLTLLTL2LL1LLL1LL1L"
@@ -34,7 +33,6 @@ function availableMove(arrayBoard, y, x) {
    var isValid = 0;
    for (var i=-1; i<2; i+=2)// Horizontal Entries
    {
-
       if (x + i >= 0 && x + i <= 5 && arrayBoard[y][x+i] == "E")
       {
          isValid = 1;
@@ -66,6 +64,7 @@ function availableMove(arrayBoard, y, x) {
             }
          }
       }
+
       if (x + j >= 1 && x + j <= 5 && arrayBoard[y+j][x] == "E")
       {
          isValid = 1;
@@ -74,15 +73,16 @@ function availableMove(arrayBoard, y, x) {
    return isValid;
 
 }
-
 function statusOfGame(arrayBoard) {
    var numberOfTigersTrapper = 0;
    var noOfTigers = 0;
    var noOfLambs = 0;
+   var noOfEmpty = 0;
    for (var i=0; i<5; i++)
    {
       for (var j=0; j<6; j++)
       {
+
          if (arrayBoard[i][j] == "T")
          {
             noOfTigers++;
@@ -101,7 +101,7 @@ function statusOfGame(arrayBoard) {
    if (numberOfTigersTrapper == 3)
    {
       return 1;
-   }else if (noOfLambs < 6)
+   }else if (noOfTigers == 3 && noOfLambs < 6)
    {
       return -1;
    }
@@ -129,13 +129,13 @@ function convertToReadable(board) {
    // [L, T, L, E, E, L]
    // [L, E, L, L, L, E]
    // [0, L, L, E, L, 0]
+   // [1] Denotes who's turn
    // E - Represents an Empty spot.
 
    var regexStr = board.match(/[a-z]+|[^a-z]+/gi); // [ 'LLTLLTLLTL', '2', 'LL', '1', 'LLL', '1', 'LL', '1', 'L' ]
    let arrLen = regexStr.length
-
    var arrayStr = "";
-   for (var i=0; i< arrLen; i++)
+   for (var i=0; i< arrLen - 1; i++)
    {
       // True if Str
       // False if Int
@@ -157,10 +157,11 @@ function convertToReadable(board) {
       }
 
    }
-
+   arrayStr += " "
+   arrayStr += regexStr[arrLen-1][1]
 
    let arrayStrLen = arrayStr.length
-   arrayBoard = [[], [], [], [], []]
+   arrayBoard = [[], [], [], [], [], []]
 
 
    arrayBoard[0].push("0", "0", arrayStr[0], "0", "0", "0")
@@ -168,7 +169,7 @@ function convertToReadable(board) {
    arrayBoard[2].push(arrayStr[7], arrayStr[8], arrayStr[9], arrayStr[10], arrayStr[11], arrayStr[12])
    arrayBoard[3].push(arrayStr[13], arrayStr[14], arrayStr[15], arrayStr[16], arrayStr[17], arrayStr[18])
    arrayBoard[4].push("0", arrayStr[19], arrayStr[20], arrayStr[21], arrayStr[22], "0")
-
+   arrayBoard[5].push(parseInt(arrayStr[24]))
    return arrayBoard
 
 
@@ -195,7 +196,6 @@ function convertToReadable(board) {
 
 
 }
-
 function convertToFen(arrayBoard) {
    // convert arrayBoard to FEN STRING
 
@@ -232,40 +232,111 @@ function convertToFen(arrayBoard) {
          
       }
    }
+   board += " " + arrayBoard[5][0].toString()
    return board;
-
-  
 }
-  
+function makemove(arrayBoard, final, initial) { 
 
-function makemove(arrayBoard, location) { 
-   if (location == 1)
+   // Makes a move
+   // Changes arrayBoard
+   // Location ranges from 1 - 23
+   // Doesn't check if valid
+
+   var converter = [[0, 2], [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], 
+   [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], 
+   [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], 
+   [4, 1], [4, 2], [4, 3], [4, 4]]
+   let destCoordinate = converter[final-1]
+   let initCoordinate = converter[initial-1]
+   let player = arrayBoard[initCoordinate[0]][initCoordinate[1]]
+   var oponent = ""
+   if (player == "L")
    {
+      oponent = "T"
+   }else {
+      oponent = "L"
    }
-   
-    // ** This function will not return anything,
-    // it will change the arangement of the board
-    //
 
-    return board;
+   var isValid = false
+
+   var capturedLocation = []
+   var isCaptured = false
+   if (destCoordinate[0] == initCoordinate[0])
+   {
+      if (initCoordinate[1] + 2 == destCoordinate[1])
+      {
+         isCaptured = true
+         capturedLocation = [destCoordinate[0], initCoordinate[1] + 1]
+      }else if (initCoordinate[1] - 2 ==  destCoordinate[1])
+      {
+         isCaptured = true
+         capturedLocation = [destCoordinate[0], initCoordinate[1] - 1]
+
+      }
+   }else if (destCoordinate[1] == initCoordinate[1])
+   {
+      if (initCoordinate[0] + 2 == destCoordinate[0])
+      {
+         isCaptured = true
+
+         capturedLocation = [initCoordinate[0] + 1, destCoordinate[1]]
+
+      }else if (initCoordinate[0] - 2 ==  destCoordinate[0])
+      {
+         isCaptured = true
+         capturedLocation = [initCoordinate[0] - 1, destCoordinate[1]]
+
+      }
+   }
+   if (destCoordinate[0] == initCoordinate[0])
+   {
+      if (initCoordinate[1] + 1 == destCoordinate[1])
+      {
+         isValid = true
+      }else if (initCoordinate[1] - 1 ==  destCoordinate[1])
+      {
+         isValid = true
+      }
+   }else if (destCoordinate[1] == initCoordinate[1])
+   {
+      if (initCoordinate[0] + 1 == destCoordinate[0])
+      {
+         isValid = true
+      }else if (initCoordinate[0] - 1 ==  destCoordinate[0])
+      {
+         isValid = true
+      }
+   }
+   if (capturedLocation != [] && player == "L")
+   {
+      isCaptured = false
+   }
+   console
+   if (arrayBoard[destCoordinate[0]][destCoordinate[1]] == "E" && isValid == true)
+   {
+      arrayBoard[destCoordinate[0]][destCoordinate[1]] = player
+      arrayBoard[initCoordinate[0]][initCoordinate[1]] = "E"
+
+   }else if (isCaptured == true && capturedLocation != [] && player == "T")
+   {
+      
+      if (arrayBoard[capturedLocation[0]][capturedLocation[1]] == oponent)
+      {
+         arrayBoard[capturedLocation[0]][capturedLocation[1]] = "E"
+         arrayBoard[destCoordinate[0]][destCoordinate[1]] = player
+         arrayBoard[initCoordinate[0]][initCoordinate[1]] = "E"
+
+      }
+   }
+   return arrayBoard
+
   
-}
-  
- 
-  
-  
+} 
 function determineoptimal(board) { 
    // ** This function will determine what the most
    // optimal move would be given any current position
   
-}
-  
- 
-  
-  
- 
-  
-  
+} 
 function createdatabase() {
   
     // This module will be connected to the SQL database,
@@ -278,34 +349,91 @@ function createdatabase() {
     // FEN
     // Time
     // Rating of Player
-  
 }
-  
-function gameplay() {
-   
-  
-}
+function checkForRepetition(localDatabase)
+{
+   if (localDatabase.length < 10)
+   {
+      return false
+   }
+   var count = 0;
+   var firstIter = localDatabase[localDatabase.length - 1]
+   var secondIter = localDatabase[localDatabase.length - 2]
+   var thirdIter = localDatabase[localDatabase.length - 3]
+   var fourthIter = localDatabase[localDatabase.length - 4]
+   var fifthIter = localDatabase[localDatabase.length - 5]
+   var sixthIter = localDatabase[localDatabase.length - 6]
+   if (firstIter == thirdIter && thirdIter == fifthIter)
+   {
+      count++;
+   }
+   if (secondIter == fourthIter && fourthIter == sixthIter)
+   {
+      count++
+   }
 
-   // [0, 0, L, 0, 0, 0]
-   // [L, T, L, L, T, L] 1 - 5 (exclusive) connect to L (top row)
-   // [L, T, L, E, E, L]
-   // [L, E, L, L, L, E]
-   // [0, L, L, E, L, 0]
+   if (count == 2)
+   {
+      return true
+   }
+   return false
+}
+function gameplay() {
+   var board = "23"
+   var arrayBoard = convertToReadable(board)
+   var localDatabase = []
+
+   for (var iter=0; iter<18; iter++)
+   {
+      //Make Move
+      localDatabase.push(convertToFen(arrayBoard))
+   }
+   while (statusOfGame(arrayBoard) == 0 && checkForRepetition(localDatabase) == false)
+   {
+      localDatabase.push(convertToFen(arrayBoard))
+   }
+   if (statusOfGame(arrayBoard) == 1)
+   {
+      //Tiger's Win
+      return 1;
+   }else if (statusOfGame(arrayBoard) == -1)
+   {
+      // Lamb's Win
+      return -1;
+   }else {
+      // Draw from repetition
+      return 0;
+   }
+  
+}
 
 var test1 = [["0", "0", "L", "0", "0", "0"], ["L", "T", "L", "L", "T", "L"], ["L", "T", "L", "E", "E", "L"], ["L", "E", "L", "L", "L", "E"], ["0", "L", "L", "E", "L", "0"]]
 var test2 = [["0", "0", "L", "0", "0", "0"], 
-             ["L", "T", "L", "L", "T", "L"], 
-             ["L", "T", "L", "L", "L", "L"], 
-             ["L", "L", "L", "L", "L", "L"], 
-             ["0", "L", "L", "L", "L", "0"]]
+             ["E", "T", "E", "E", "T", "L"], 
+             ["E", "T", "E", "E", "E", "E"], 
+             ["E", "E", "E", "E", "E", "E"], 
+             ["0", "E", "E", "E", "E", "0"]]
 
 var test2 = [["0", "0", "E", "0", "0", "0"], 
              ["E", "E", "E", "E", "E", "E"], 
              ["E", "E", "E", "E", "E", "E"], 
              ["E", "E", "E", "E", "E", "E"], 
              ["0", "E", "E", "E", "L", "0"]]
-var fen = "LLTLLTLLTL2LL1LLL1LL1L"
+var fen = "LLTLLTLLTL2LL1LLL1LL1L 1"
+
+var test3 = [["0", "0", "E", "0", "0", "0"], 
+             ["T", "L", "E", "E", "E", "E"], 
+             ["L", "E", "E", "E", "E", "E"], 
+             ["E", "E", "E", "E", "E", "E"], 
+             ["0", "E", "E", "E", "E", "0"]]
+
+console.log(test3)
+
+console.log(makemove(test3, 14, 2))
 
 // console.log(convertToFen(test2))
 // console.log(statusOfGame(test2))
-console.log(convertToFen(convertToReadable(fen)))
+
+// console.log(statusOfGame(test3))
+// console.log(convertToFen(convertToReadable(fen)))
+
--- tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
-
-   arrayBoard[1].push(arrayStr[1], arrayStr[2], arrayStr[3], arrayStr[4], arrayStr[5], arr
